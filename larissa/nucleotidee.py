191978ca076d4cd27f9eb8decdf7b8c96de52ba9
larissamborg <larissamborg@github.com>
2020-06-16 14:47:08 +0000

    updated code

--- larissa/nucleotidee.py
+++ larissa/nucleotidee.py
@@ -1,4 +1,14 @@
-f=open('small-genome')
-lines=f.readline()
-print(len(lines))
+f=open('../../small-genome', 'r')
+line0=f.readline()
+line1=f.readline()
+line2=f.readline()
+line3=f.readline()
+
+count0=len(line1.rstrip('/n'))
+count1=len(line2.rstrip('/n'))
+count2=len(line3.rstrip('/n'))
+
+result=count0+count1+count2-3
+print(result)
+
 
